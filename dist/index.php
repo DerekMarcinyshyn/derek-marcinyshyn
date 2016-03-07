1b85dec4af1783607f5a0233898dae97288e3021
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-03-06 21:00:41 -0800

    update tracking code

--- dist/index.php
+++ dist/index.php
@@ -134,7 +134,7 @@
         <script src="app.js"></script>
         <script>
             window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+            ga('create','UA-74761685-2','auto');ga('send','pageview')
         </script>
         <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     </body>
